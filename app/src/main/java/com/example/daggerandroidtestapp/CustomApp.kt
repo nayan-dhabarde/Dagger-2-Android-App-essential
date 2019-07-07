0459d294f6bac31b674eea54cb692793f0c0da01
Nayan <ndhabarde11@gmail.com>
2019-07-07 22:32:14 +0530

    first commit

---
+++ app/src/main/java/com/example/daggerandroidtestapp/CustomApp.kt
@@ -0,0 +1,23 @@
+package com.example.daggerandroidtestapp
+
+import android.app.Activity
+import android.app.Application
+import com.example.daggerandroidtestapp.di.AppInjector
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import javax.inject.Inject
+
+class CustomApp: Application(), HasActivityInjector {
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
+
+    override fun onCreate() {
+        super.onCreate()
+        AppInjector.init(this)
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> = dispatchingAndroidInjector
+
+
+}
